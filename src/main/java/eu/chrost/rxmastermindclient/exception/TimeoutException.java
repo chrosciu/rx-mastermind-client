11a1d981f3a87ff5498a0af2dda29c52adb0f3f3
Marcin Chrost <marcin.chrost@contractors.roche.com>
2021-04-20 22:42:49 +0200

    workshop bare version

--- src/main/java/eu/chrost/rxmastermindclient/exception/TimeoutException.java
+++
@@ -1,7 +0,0 @@
-package eu.chrost.rxmastermindclient.exception;
-
-public class TimeoutException extends RuntimeException {
-    public TimeoutException(String message) {
-        super(message);
-    }
-}
--- src/main/java/eu/chrost/rxmastermindclient/runner/GameRunner.java
+++ src/main/java/eu/chrost/rxmastermindclient/runner/GameRunner.java
@@ -1,31 +1,19 @@
 package eu.chrost.rxmastermindclient.runner;
 
-import eu.chrost.rxmastermindclient.service.GameService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.CountDownLatch;
-
 @Component
-@Profile("!test")
 @RequiredArgsConstructor
 @Slf4j
 public class GameRunner implements CommandLineRunner {
-    private final GameService gameService;
 
     @Override
     public void run(String... args) throws Exception {
-        CountDownLatch countDownLatch = new CountDownLatch(1);
-        gameService.getResults()
-                .doFinally(signalType -> countDownLatch.countDown())
-                .subscribe(
-                        System.out::println,
-                        e -> log.warn("Error:", e)
-                );
-        countDownLatch.await();
+        log.info("IMPLEMENT ME !");
+        //TODO Implement game logic
     }
 
 }
