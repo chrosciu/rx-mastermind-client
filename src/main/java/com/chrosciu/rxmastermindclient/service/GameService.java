05ce651f7a03afb43df64518c826746c2c12124f
Marcin Chrost <mchrost82@gmail.com>
2023-12-22 16:32:05 +0100

    Partial implementation of GameService

--- src/main/java/com/chrosciu/rxmastermindclient/service/GameService.java
+++ src/main/java/com/chrosciu/rxmastermindclient/service/GameService.java
@@ -11,11 +11,17 @@ import reactor.core.scheduler.Schedulers;
 public class GameService {
     private final SessionService sessionService;
 
+    private static final String SUCCESS_RESULT = "40";
+
     public Flux<String> getResults(Flux<String> samples) {
         Mono<Long> sessionId = sessionService.getSessionId();
         Flux<String> results = sessionId.flatMapMany(id -> samples
                 .subscribeOn(Schedulers.boundedElastic())
                 .flatMap(sample -> sessionService.getResult(id, sample))
+                .takeUntil(SUCCESS_RESULT::equals)
+                .doOnSubscribe(s -> System.out.println("Please enter samples: "))
+                .doOnComplete(() -> System.out.println("Game finished"))
+                .concatWith(sessionService.destroySession(id).then(Mono.empty()))
         );
         return results;
     }
